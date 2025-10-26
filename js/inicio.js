5c3e847b5c7ce3f1ecdc777e4e9c992f847313e7
Ezecuci <ezecuci@gmail.com>
2025-10-25 21:39:56 -0300

    primer prueba js - slider index

---
+++ js/inicio.js
@@ -0,0 +1,31 @@
+const contenedorDeImagenes = document.querySelector('.top__image');
+const imagenesDelSlide = document.querySelectorAll('.top__image img');
+const btnBack = document.querySelector('.top__image-btn.back');
+const btnNext = document.querySelector('.top__image-btn.next');
+
+let index = 0;
+
+function desplazarImagen() {
+
+    contenedorDeImagenes.style.transform = `translateX(${-index * 100}%)`;
+}
+
+btnNext.addEventListener('click' , () => {
+
+    index ++;
+    if(index >= imagenesDelSlide.length) index = 0;
+    desplazarImagen();
+});
+
+btnBack.addEventListener('click' , () => {
+
+    index --;
+    if(index < 0) index = imagenesDelSlide.length - 1;
+    desplazarImagen();
+});
+
+setInterval(() => {
+  index++;
+  if (index >= imagenesDelSlide.length) index = 0;
+  desplazarImagen();
+}, 5000);
