d7757df1d30098dfd6f448257141a1bbc1bf00f5
Ezecuci <ezecuci@gmail.com>
2025-11-06 00:44:03 -0300

    script inicio de sesion funcionando

--- js/iniciar-sesion.js
+++ js/iniciar-sesion.js
@@ -1,14 +1,51 @@
 
-// const form = document.querySelector('.login__form');
-// const formState = document.querySelector('.form_state');
-// formState.style.textAling = 'center';
+const form = document.querySelector('.login__form');
 
-// function sePuedeIniciarSesion(){
-
-//     const email = document.getElementById('email').value;
-//     const contrasenia = document.getElementById('password').value;
-//     const usuarios = JSON.parse(sessionStorage.getItem('usuarios'));
+const inputEmail = document.getElementById('email');
+const inputContrasenia = document.getElementById('password');
 
 
+form.addEventListener('submit', (event) => {
 
-// }
+    event.preventDefault();
+
+    const email = inputEmail.value.trim().toLowerCase();
+    const contrasenia = inputContrasenia.value;
+
+    if(sePuedeIniciarSesion(email, contrasenia)){
+
+        form.reset();
+
+        window.location.href = './index.html';
+    }
+
+});
+
+function sePuedeIniciarSesion(email, password){
+
+    const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+    for ( const actual of usuarios){
+
+        if(actual.email === email){
+
+            if( actual.password === password){
+
+                return true;
+            }else {
+
+            alert('contraseña incorrecta, intentelo nuevamente');
+            
+            inputContrasenia.focus();
+            return false;
+            
+            }
+        }
+
+    }
+
+    alert('ingreso invalido, el email ingresado no se encuentra registrado');
+    inputEmail.focus();
+    return false;
+
+}
