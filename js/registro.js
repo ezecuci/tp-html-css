fa1d65f186236830a563e5827581dc6d6654c258
Joaquin Gielis <joaquingielis05@gmail.com>
2025-11-10 01:17:30 -0300

    mejora registro de usuario

--- js/registro.js
+++ js/registro.js
@@ -1,5 +1,4 @@
 
-
 // codigo para modificar el label y placeholder de tipo de usuario
 // con el input radio 
 
@@ -14,13 +13,8 @@ const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
 
 
 const form = document.querySelector('.registro__form');
-const labelApellido = document.querySelector('.apellido');
-const inputApellido = document.getElementById('apellido');
-
-
-const labelNombre = document.querySelector('.nombre');
-const inputNombre = document.getElementById('nombre');
-
+const labelNombreApellido = document.querySelector('.nombre-apellido');
+const inputNombreApellido = document.getElementById('nombre-apellido');
 const inputEmail = document.getElementById('email');
 const inputPassword = document.getElementById('password');
 const inputPassword2 = document.getElementById('password2');
@@ -29,25 +23,13 @@ form.addEventListener('change', (e) => {
 
     const target = e.target;
 
-    if(!target.matches('.registro__radioButton')) return;
-        
+    if(!target.matches('.registro__radioButton')) return;   
     if(target.value === 'personal'){
-
-        labelApellido.textContent = 'Apellido';
-        inputApellido.placeholder = 'Apellido';
-
-        inputNombre.style.display = '';
-        labelNombre.style.display = '';
-        inputNombre.setAttribute('required', '');   
-
+        labelNombreApellido.textContent = 'Nombre y apellido';
+        inputNombreApellido.placeholder = 'Nombre y apellido';
     }else if (target.value === 'empresa') {
-
-        labelApellido.textContent = 'Nombre de empresa'; 
-        inputApellido.placeholder = 'Nombre de empresa';
-
-        inputNombre.style.display = 'none';
-        labelNombre.style.display = 'none';
-        inputNombre.removeAttribute('required');   
+        labelNombreApellido.textContent = 'Nombre de empresa'; 
+        inputNombreApellido.placeholder = 'Nombre de empresa';
     }
 });
 
@@ -58,81 +40,76 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const tipo = document.querySelector('.registro__radioButton:checked').value;
-
-    let nombre = null;
-
-    if(tipo === 'personal'){
-        nombre = inputNombre.value.trim().toUpperCase();
-    }
-
-    const apellido = inputApellido.value.trim().toUpperCase(); 
-    const email = inputEmail.value.trim().toLowerCase();;
+    const nombreIngresado = inputNombreApellido.value.trim();
+    const email = inputEmail.value.trim().toLowerCase();
     const password = inputPassword.value;
     const password2 = inputPassword2.value;
-    
-    const valido = validacionDeIngreso(apellido, nombre, email, password, password2, tipo);
 
-    if(!valido) return;
-        
-    const nuevoUsuario = {apellido, nombre, email, password, tipo};
-    usuarios.push(nuevoUsuario);
-    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+    if(tipo === 'personal'){
+        if (!nombreIngresado.includes(' ')) {
+            modal.mostrarMensaje('Ingresá tu nombre y apellido', () => {
+              inputNombre.focus();
+            });
+            return;}
+        }else {
+        if (nombreIngresado.length < 2) {
+          modal.mostrarMensaje('Ingresá el nombre de la empresa', () => {
+            inputNombre.focus();
+          });
+          return;
+        }
+      }
 
-    modal.mostrarMensaje('Usuario registrado correctamente', () => {
-        form.reset();
-        window.location.href = './index.html';
+      if (!validador.esUnEmailValido(email)) {
+        modal.mostrarMensaje(
+          'El email ingresado no es válido.',
+          () => {
+            inputEmail.focus();
+          }
+        );
+        return;
+      }
+
+      const existe = usuarios.some((u) => u.email === email);
+      if (existe) {
+        modal.mostrarMensaje('Ya existe una cuenta con ese email. Usá otro correo o iniciá sesión.',() => {
+        inputEmail.focus();
+      }
+    );
+    return;
+  }
+  
+  if (!validador.esPasswordValida(password)) {
+    modal.mostrarMensaje('La contraseña no es válida.<br>Debe tener mayúscula, minúscula, número y símbolo, y al menos 8 caracteres.', () => {
+        inputPassword.focus();
+      }
+    );
+    return;
+  }
+
+  if (!validador.sonIguales(password, password2)) {
+    modal.mostrarMensaje('Las contraseñas no coinciden', () => {
+      inputPassword2.focus();
     });
+    return;
+  }
 
- });
 
+  const nuevoUsuario = {tipo, nombre: nombreIngresado.toUpperCase(), email, password};
 
+  usuarios.push(nuevoUsuario);
+  localStorage.setItem('usuarios', JSON.stringify(usuarios));
+  sessionStorage.setItem(
+    'sesionActiva',
+    JSON.stringify({
+      email,
+      nombre: nombreIngresado,
+    })
+  );
+  modal.mostrarMensaje('Usuario registrado correctamente!', () => {
+    form.reset();
+    window.location.href = './index.html';
+  });
+});
 
-function validacionDeIngreso(apellido, nombre, email, password, password2, tipo){
     
-    if(!validador.esNombrePropioValido(apellido)){
-        modal.mostrarMensaje('El apellido ingresado no es valido', () => {
-        inputApellido.focus();
-        });
-        return false;
-    }
-
-    if(tipo === 'personal' && !validador.esNombrePropioValido(nombre)){
-        modal.mostrarMensaje('El nombre ingresado no es valido', () => {
-        inputNombre.focus();
-        });
-        return false;
-    }
-
-    if(!validador.esUnEmailValido(email)){
-        modal.mostrarMensaje('el email ingresado no es valido\nUtilice el siguiente formato: usuario@ejemplo.com', () => {
-        inputEmail.focus();
-        });
-        return false;
-    }
-
-    const existe = usuarios.some(u => u.email === email);
-    if (existe) {
-        modal.mostrarMensaje('Ya existe una cuenta con ese email. Usá otro correo o iniciá sesión', () => {
-        inputEmail.focus();
-        });
-        return false;
-    }
-
-    if(!validador.esPasswordValida(password)){
-        modal.mostrarMensaje('La contraseña ingresada no es valida', () => {
-        inputPassword.focus();
-        });
-        return false;
-    }
-
-    if(!validador.sonIguales(password, password2)){
-        modal.mostrarMensaje('las contraseñas ingresadas no coinciden', () => {
-        inputPassword.focus();
-        });
-        return false;
-    }
-
-    return true;
-
-}
-
